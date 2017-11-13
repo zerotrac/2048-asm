255b43deb4c61061391bcc310573eae023b31ab2
zerotrac <thss15_chensx@163.com>
2017-11-13 21:18:22 +0800

    Initial commit, a simple Win32 program written in asm

---
+++ 2048/WinMain.asm
@@ -0,0 +1,91 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+include windows.inc
+include gdi32.inc
+include user32.inc
+include kernel32.inc
+
+includelib gdi32.lib
+includelib user32.lib
+includelib kernel32.lib
+
+.data?
+hInstance dd ?
+hWinMain dd ?
+
+.const
+szClassName db 'MyClass', 0
+szCaptionMain db '2048-asm', 0
+szText db 'Win32 Assembly, Simple and powerful!', 0
+
+.code
+_ProcWinMain proc uses ebx edi esi,
+    hWnd: HWND, uMsg: UINT, wParam: WPARAM, lParam: LPARAM
+    
+    local @stPs: PAINTSTRUCT
+    local @stRect: RECT
+    local @hDc
+                                                                               
+    mov eax,uMsg
+    .if eax == WM_PAINT
+        invoke BeginPaint, hWnd, addr @stPs
+        mov @hDc, eax
+        invoke GetClientRect, hWnd, addr @stRect
+        invoke DrawText, @hDc, addr szText, -1, addr @stRect, DT_SINGLELINE or DT_CENTER or DT_VCENTER
+        invoke EndPaint,hWnd,addr @stPs
+    .elseif eax == WM_CLOSE
+        invoke DestroyWindow, hWinMain
+        invoke PostQuitMessage, NULL
+    .else
+        invoke DefWindowProc, hWnd, uMsg, wParam, lParam
+        ret
+    .endif
+                                                                      
+    xor eax,eax
+    ret
+_ProcWinMain endp
+
+_WinMain proc
+    local @stWndClass: WNDCLASSEX
+    local @stMsg: MSG
+                                                                               
+    invoke GetModuleHandle, NULL
+    mov hInstance,eax
+
+    invoke RtlZeroMemory, addr @stWndClass, sizeof @stWndClass
+                                                                               
+    invoke LoadCursor, 0, IDC_ARROW
+    mov @stWndClass.hCursor, eax
+    push hInstance
+    pop @stWndClass.hInstance
+    mov @stWndClass.cbSize, sizeof WNDCLASSEX
+    mov @stWndClass.style, CS_HREDRAW or CS_VREDRAW
+    mov @stWndClass.lpfnWndProc, offset _ProcWinMain
+    mov @stWndClass.hbrBackground, COLOR_WINDOW + 1
+    mov @stWndClass.lpszClassName, offset szClassName
+
+    invoke RegisterClassEx, addr @stWndClass
+                                                                               
+    invoke CreateWindowEx, WS_EX_CLIENTEDGE, offset szClassName, offset szCaptionMain, WS_OVERLAPPEDWINDOW, 100, 100, 600, 400, NULL, NULL, hInstance, NULL
+    mov hWinMain, eax
+
+    invoke ShowWindow, hWinMain, SW_SHOWNORMAL
+
+    invoke UpdateWindow, hWinMain
+                                                                               
+    .while TRUE
+        invoke GetMessage, addr @stMsg,NULL, 0, 0
+        .break .if eax == 0
+        invoke TranslateMessage, addr @stMsg
+        invoke DispatchMessage, addr @stMsg
+    .endw
+    ret
+
+_WinMain endp
+
+start:
+    call _WinMain
+    invoke ExitProcess, NULL
+end start
